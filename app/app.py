f238d19fcfe7f5ea90e1ec43aff81b2e6d43096a
企鹅 <qie@qiedeMacBook-Pro.local>
2024-06-14 16:35:21 +0800

    Update app with new features

--- app/app.py
+++ app/app.py
@@ -4,12 +4,12 @@ import streamlit as st                          # 构建Web应用
 
 # streamlit run app/app.py
 
-# 使用HTML和CSS隐藏上标和注脚
+# 使用HTML和CSS隐藏上标和注脚，包括 "Manage app" 按钮
 hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
-            header {visibility: hidden;}
+            .viewerBadge_container__1QSob {visibility: hidden;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
